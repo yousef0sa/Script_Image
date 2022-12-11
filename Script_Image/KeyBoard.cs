99b469cc9b467fc7e36ce28ef4fac9cf19bb97d1
yousef alslami <yosef0sa@gmail.com>
2022-12-12 00:03:25 +0300

    Create Keyboard class

---
+++ Script_Image/KeyBoard.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ScriptImage
+{
+    public class KeyBoard
+    {
+
+
+        
+
+
+        #region DllImport
+        [DllImport("user32.dll")]
+        private static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        #endregion
+
+        const uint WM_KEYDOWN = 0x100;
+        const uint WM_KEYUP = 0x0101;
+
+
+        public static void Press(IntPtr hWnd, Keys keys, int delayTime = 1)
+        {
+            PostMessage(hWnd, WM_KEYDOWN, (IntPtr)(keys), IntPtr.Zero);
+            Thread.Sleep(delayTime + 000);
+            PostMessage(hWnd, WM_KEYUP, (IntPtr)(keys), IntPtr.Zero);
+        }
+
+    }
+}
