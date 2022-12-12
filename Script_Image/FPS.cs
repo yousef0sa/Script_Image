673ca3d960e919627306ff94edc647866c0fe261
yousef alslami <yosef0sa@gmail.com>
2022-12-12 17:37:11 +0300

    Make FPS class

---
+++ Script_Image/FPS.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System;
+
+namespace ScriptImage
+{
+    public class FPS
+    {
+        private static  DateTime _lastCheckTime = DateTime.Now;
+        private static long _frameCount = 0;
+
+
+        private static DateTime LastCheckTime
+        {
+            get { return _lastCheckTime; }
+            set { _lastCheckTime = value; }
+        }
+        
+        // called whenever a map is updated
+        private static void OnMapUpdated()
+        {
+            Interlocked.Increment(ref _frameCount);
+        }
+
+        // called every once in a while
+        public static double GetFps()
+        {
+            OnMapUpdated();
+
+            double secondsElapsed = (DateTime.Now - LastCheckTime).TotalSeconds;
+            long count = Interlocked.Exchange(ref _frameCount, 0);
+            double fps = count / secondsElapsed;
+            LastCheckTime = DateTime.Now;
+            return fps;
+        }
+    }
+}
