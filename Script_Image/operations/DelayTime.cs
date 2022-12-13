ede1a995ae59258e5779334d5d0f1d0866a41c77
yousef alslami <yosef0sa@gmail.com>
2022-12-13 20:50:19 +0300

    Make Mouse and DealyTime Classes

---
+++ Script_Image/operations/DelayTime.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+
+namespace ScriptImage
+{
+    public class DelayTime
+    {
+        public static void Delay(double Time)
+        {
+            Time *= 1000;
+            Thread.Sleep(((int)Time));
+        }
+    }
+}
---
+++ Script_Image/operations/Mouse.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ScriptImage
+{
+    public class Mouse : DllHolder
+    {
+        public enum WMessages : uint
+        {
+            WM_RBUTTONDOWN = 0x204, //Right mousebutton down
+            WM_RBUTTONUP = 0x205, //Right mousebutton up
+            WM_LBUTTONDOWN = 0x201, //Left mousebutton down
+            WM_LBUTTONUP = 0x202, //Left mousebutton up   
+        }
+
+        public static void Right_Click(IntPtr hWnd, (int x,int y) Location,  double delayTime = 1)
+        {
+
+            PostMessage(hWnd, (uint)WMessages.WM_RBUTTONDOWN, IntPtr.Zero, MakeLParam(Location.x, Location.y));
+            DelayTime.Delay(delayTime);
+            PostMessage(hWnd, (uint)WMessages.WM_RBUTTONUP, IntPtr.Zero, MakeLParam(Location.x, Location.y));
+        }
+
+        public static void Left_Click(IntPtr hWnd, (int x, int y) Location, double delayTime = 1)
+        {
+
+            PostMessage(hWnd, (uint)WMessages.WM_LBUTTONDOWN, IntPtr.Zero, MakeLParam(Location.x, Location.y));
+            DelayTime.Delay(delayTime);
+            PostMessage(hWnd, (uint)WMessages.WM_LBUTTONUP, IntPtr.Zero, MakeLParam(Location.x, Location.y));
+        }
+
+        private static int MakeLParam(int LoWord, int HiWord)
+        {
+            return (int)((HiWord << 16) | (LoWord & 0xFFFF));
+        }
+
+
+
+
+    }
+}
